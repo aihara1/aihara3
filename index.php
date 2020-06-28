f9a79f4b7d3822703f0a4ad21a7edc7f32002cbd
aihara <petr_konyuk@mail.ru>
2020-06-28 16:13:06 +0300

    new

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+echo(getcwd());
+$dbh = new PDO('mysql:host=localhost;dbname=aihara', "root", "");
+
+$res = $dbh->query("SELECT * FROM users");
+foreach($res->fetch() as $key => $val ) {
+    echo $key . "===" . $val . "<br />";
+};
+?>
